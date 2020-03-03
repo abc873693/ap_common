b3c3e7cf3925a3f912fee8c152dcb6bbafd93e68
RainVisitor <abc873693@gmail.com>
2020-03-04 00:27:50 +0800

    Add icon class

---
+++ lib/resources/ap_icon.dart
@@ -0,0 +1,329 @@
+import 'package:flutter/material.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+
+class ApIcon {
+  static const String FILLED = 'filled';
+  static const String OUTLINED = 'outlined';
+
+  static String code = ApIcon.OUTLINED;
+
+  static IconData get directionsBus {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.directions_bus;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.directionsBus;
+    }
+  }
+
+  static IconData get classIcon {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.class_;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.classIcon;
+    }
+  }
+
+  static IconData get assignment {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.assignment;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.assignment;
+    }
+  }
+
+  static IconData get accountCircle {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.account_circle;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.accountCircle;
+    }
+  }
+
+  static IconData get school {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.school;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.school;
+    }
+  }
+
+  static IconData get apps {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.apps;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.apps;
+    }
+  }
+
+  static IconData get calendarToday {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.calendar_today;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.calendarToday;
+    }
+  }
+
+  static get edit {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.edit;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.edit;
+    }
+  }
+
+  static get dateRange {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.date_range;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.dateRange;
+    }
+  }
+
+  static get info {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.info;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.info;
+    }
+  }
+
+  static get face {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.face;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.face;
+    }
+  }
+
+  static get settings {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.settings;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.settings;
+    }
+  }
+
+  static IconData get powerSettingsNew {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.power_settings_new;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.powerSettingsNew;
+    }
+  }
+
+  static get permIdentity {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.perm_identity;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.permIdentity;
+    }
+  }
+
+  static IconData get accessTime {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.access_time;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.accessTime;
+    }
+  }
+
+  static IconData get keyboardArrowDown {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.keyboard_arrow_down;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.keyboardArrowDown;
+    }
+  }
+
+  static get offlineBolt {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.offline_bolt;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.offlineBolt;
+    }
+  }
+
+  static IconData get error {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.error;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.error;
+    }
+  }
+
+  static IconData get fiberNew {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.fiber_new;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.fiberNew;
+    }
+  }
+
+  static IconData get phone {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.phone;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.phone;
+    }
+  }
+
+  static IconData get codeIcon {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.code;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.code;
+    }
+  }
+
+  static IconData get cancel {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.cancel;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.cancel;
+    }
+  }
+
+  static IconData get check {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.check;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.check;
+    }
+  }
+
+  static IconData get arrowDropUp {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.arrow_drop_up;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.arrowDropUp;
+    }
+  }
+
+  static IconData get arrowDropDown {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.arrow_drop_down;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.arrowDropDown;
+    }
+  }
+
+  static IconData get chevronLeft {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.chevron_left;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.chevronLeft;
+    }
+  }
+
+  static IconData get chevronRight {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.chevron_right;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.chevronRight;
+    }
+  }
+
+  static IconData get person {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.person;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.person;
+    }
+  }
+
+  static IconData get exitToApp {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.exit_to_app;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.exitToApp;
+    }
+  }
+
+  static IconData get warning {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.warning;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.warning;
+    }
+  }
+
+  static IconData get folder {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.folder;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.folder;
+    }
+  }
+
+  static IconData get insertDriveFile {
+    switch (ApIcon.code) {
+      case ApIcon.FILLED:
+        return Icons.insert_drive_file;
+      case ApIcon.OUTLINED:
+      default:
+        return OMIcons.insertDriveFile;
+    }
+  }
+}
