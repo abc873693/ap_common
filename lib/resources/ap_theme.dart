b10c6d9ce871c5ebc48f03dcc9cbee50a0d46db5
RainVisitor <abc873693@gmail.com>
2020-03-05 00:07:32 +0800

    Add system mode in ap theme
    Change ApTheme to InheritedWidget

--- lib/resources/ap_theme.dart
+++ lib/resources/ap_theme.dart
@@ -2,27 +2,176 @@ import 'package:flutter/material.dart';
 
 import 'ap_colors.dart';
 
-class ApTheme {
+class ApTheme extends InheritedWidget {
+  final ThemeMode themeMode;
+
+  ApTheme(this.themeMode, {Widget child}) : super(child: child);
+
+  static ApTheme of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType();
+  }
+
+  @override
+  bool updateShouldNotify(ApTheme oldWidget) {
+    return true;
+  }
+
   static const String DARK = 'dark';
   static const String LIGHT = 'light';
+  static const String SYSTEM = 'system';
 
   static String code = ApTheme.LIGHT;
 
-  static ThemeData get data {
-    switch (ApTheme.code) {
-      case ApTheme.DARK:
-        return dark;
-      case ApTheme.LIGHT:
+  Brightness get _brightness {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return WidgetsBinding.instance.window.platformBrightness;
+      case ThemeMode.light:
+        return Brightness.light;
+      case ThemeMode.dark:
       default:
-        return light;
+        return Brightness.dark;
     }
   }
 
-  static double get drawerIconOpacity {
-    switch (ApTheme.code) {
-      case ApTheme.DARK:
+  Color get blue {
+    switch (_brightness) {
+      case Brightness.light:
+        return ApColors.blue500;
+        break;
+      case Brightness.dark:
+      default:
+        return ApColors.blueDark;
+        break;
+    }
+  }
+
+  Color get blueText {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.grey100;
+      case Brightness.light:
+      default:
+        return ApColors.blue500;
+    }
+  }
+
+  get blueAccent {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.blue300;
+      case Brightness.light:
+      default:
+        return ApColors.blue500;
+    }
+  }
+
+  get semesterText {
+    switch (_brightness) {
+      case Brightness.dark:
+        return Color(0xffffffff);
+      case Brightness.light:
+      default:
+        return ApColors.blue500;
+    }
+  }
+
+  Color get grey {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.grey200;
+      case Brightness.light:
+      default:
+        return ApColors.grey500;
+    }
+  }
+
+  Color get greyText {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.grey200;
+      case Brightness.light:
+      default:
+        return ApColors.grey500;
+    }
+  }
+
+  get disabled {
+    switch (_brightness) {
+      case Brightness.dark:
+        return Color(0xFF424242);
+      case Brightness.light:
+      default:
+        return Color(0xFFBDBDBD);
+    }
+  }
+
+  Color get calendarTileSelect {
+    switch (_brightness) {
+      case Brightness.dark:
+        return Color(0xff000000);
+      case Brightness.light:
+      default:
+        return Color(0xffffffff);
+    }
+  }
+
+  Color get yellow {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.yellow200;
+      case Brightness.light:
+      default:
+        return ApColors.yellow500;
+    }
+  }
+
+  Color get red {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.red200;
+      case Brightness.light:
+      default:
+        return ApColors.red500;
+    }
+  }
+
+  Color get bottomNavigationSelect {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.grey100;
+      case Brightness.light:
+      default:
+        return Color(0xff737373);
+    }
+  }
+
+  Color get segmentControlUnSelect {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.onyx;
+      case Brightness.light:
+      default:
+        return Color(0xffffffff);
+    }
+  }
+
+  get snackBarActionTextColor {
+    switch (_brightness) {
+      case Brightness.dark:
+        return ApColors.yellow500;
+      case Brightness.light:
+      default:
+        return ApColors.yellow500;
+    }
+  }
+
+  double get drawerIconOpacity {
+    switch (_brightness) {
+      case Brightness.light:
         return 0.75;
-      case ApTheme.LIGHT:
+
+      case Brightness.dark:
       default:
         return 1.0;
     }
@@ -32,7 +181,7 @@ class ApTheme {
         //platform: TargetPlatform.iOS,
         brightness: Brightness.light,
         appBarTheme: AppBarTheme(
-          color: ApColors.blue,
+          color: ApColors.blue500,
         ),
         accentColor: ApColors.blueText,
         unselectedWidgetColor: ApColors.grey,
