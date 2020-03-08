3301c6f0be322eaae1ebe2b77400df5ad94c55c4
RainVisitor <abc873693@gmail.com>
2020-03-08 11:41:39 +0800

    Add time code config

---
+++ lib/models/time_code.dart
@@ -0,0 +1,60 @@
+// To parse this JSON data, do
+//
+//     final timeCodeConfig = timeCodeConfigFromJson(jsonString);
+
+import 'dart:convert';
+
+class TimeCodeConfig {
+  List<TimeCode> timeCodes;
+
+  TimeCodeConfig({
+    this.timeCodes,
+  });
+
+  factory TimeCodeConfig.fromRawJson(String str) =>
+      TimeCodeConfig.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory TimeCodeConfig.fromJson(Map<String, dynamic> json) => TimeCodeConfig(
+        timeCodes: json["timeCodes"] == null
+            ? null
+            : List<TimeCode>.from(
+                json["timeCodes"].map((x) => TimeCode.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "timeCodes": timeCodes == null
+            ? null
+            : List<dynamic>.from(timeCodes.map((x) => x.toJson())),
+      };
+}
+
+class TimeCode {
+  String title;
+  String startTime;
+  String endTime;
+
+  TimeCode({
+    this.title,
+    this.startTime,
+    this.endTime,
+  });
+
+  factory TimeCode.fromRawJson(String str) =>
+      TimeCode.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory TimeCode.fromJson(Map<String, dynamic> json) => TimeCode(
+        title: json["title"] == null ? null : json["title"],
+        startTime: json["startTime"] == null ? null : json["startTime"],
+        endTime: json["endTime"] == null ? null : json["endTime"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "title": title == null ? null : title,
+        "startTime": startTime == null ? null : startTime,
+        "endTime": endTime == null ? null : endTime,
+      };
+}
