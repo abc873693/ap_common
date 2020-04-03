9465ecbade821fd1d2454b678ca518124808a881
RainVisitor <abc873693@gmail.com>
2020-04-03 17:16:29 +0800

    Add export config

--- lib/callback/general_callback.dart
+++ lib/callback/general_callback.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 
+export 'package:dio/dio.dart';
+
 class GeneralCallback {
   final Function(DioError e) onFailure;
   final Function(dynamic e) onError;
