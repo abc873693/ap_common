7c48d233a60938848c84fdc34907b1b6b5be992d
RainVisitor <abc873693@gmail.com>
2020-04-04 17:43:11 +0800

    Add general call back example instance

--- lib/callback/general_callback.dart
+++ lib/callback/general_callback.dart
@@ -1,8 +1,11 @@
 import 'package:ap_common/models/general_response.dart';
+import 'package:ap_common/utils/ap_utils.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 
 export 'package:dio/dio.dart';
+export 'package:ap_common/models/general_response.dart';
 
 class GeneralCallback {
   final Function(DioError e) onFailure;
@@ -12,4 +15,11 @@ class GeneralCallback {
     @required this.onFailure,
     @required this.onError,
   });
+
+  static GeneralCallback simple(BuildContext context) {
+    return GeneralCallback(
+      onFailure: (DioError e) => ApUtils.handleDioError(context, e),
+      onError: (GeneralResponse e) => ApUtils.showToast(context, e.message),
+    );
+  }
 }
--- lib/models/general_response.dart
+++ lib/models/general_response.dart
@@ -8,6 +8,8 @@ class GeneralResponse {
     @required this.statusCode,
     @required this.message,
   });
+  static GeneralResponse success() =>
+      GeneralResponse(statusCode: 200, message: '');
 
   static GeneralResponse unknownError() =>
       GeneralResponse(statusCode: 1000, message: '');
