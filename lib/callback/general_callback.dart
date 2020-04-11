d66d4530acc75c109a3ded3b83184e54ca3e90a5
RainVisitor <abc873693@gmail.com>
2020-04-11 21:51:14 +0800

    Add general callback on success by templete

--- lib/callback/general_callback.dart
+++ lib/callback/general_callback.dart
@@ -7,19 +7,23 @@ import 'package:flutter/widgets.dart';
 export 'package:dio/dio.dart';
 export 'package:ap_common/models/general_response.dart';
 
-class GeneralCallback {
+class GeneralCallback<T> {
   final Function(DioError e) onFailure;
   final Function(GeneralResponse e) onError;
+  final Function(T data) onSuccess;
 
   GeneralCallback({
     @required this.onFailure,
     @required this.onError,
+    @required this.onSuccess,
   });
 
-  static GeneralCallback simple(BuildContext context) {
+  factory GeneralCallback.simple(
+      BuildContext context, Function(T data) onSuccess) {
     return GeneralCallback(
       onFailure: (DioError e) => ApUtils.handleDioError(context, e),
       onError: (GeneralResponse e) => ApUtils.showToast(context, e.message),
+      onSuccess: onSuccess,
     );
   }
 }
