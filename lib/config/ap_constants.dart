f75bf2cb840fea89e40b111ad564bf999d13aef6
RainVisitor <abc873693@gmail.com>
2020-04-19 17:52:09 +0800

    Add in debug mode constant

--- lib/config/ap_constants.dart
+++ lib/config/ap_constants.dart
@@ -1,4 +1,10 @@
 class ApConstants {
+  static bool get isInDebugMode {
+    bool inDebugMode = false;
+    assert(inDebugMode = true);
+    return inDebugMode;
+  }
+
   static const TAG_STUDENT_PICTURE = "tag_student_picture";
   static const TAG_NEWS_PICTURE = "tag_news_picture";
   static const TAG_NEWS_ICON = "tag_news_icon";
