b6a8f4d71b0ea9feccfca654f600deea0f17764a
RainVisitor <abc873693@gmail.com>
2020-05-02 00:08:11 +0800

    Add export constant model in ap localization

--- lib/utils/ap_localizations.dart
+++ lib/utils/ap_localizations.dart
@@ -4,6 +4,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+export 'package:ap_common/models/ap_support_language.dart';
+
 class ApLocalizations {
   ApLocalizations(Locale locale) {
     ApLocalizations.locale = locale;
