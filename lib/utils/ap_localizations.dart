7fc84117876a450eb0d6dce9c4c50132ec688903
RainVisitor <abc873693@gmail.com>
2020-05-03 16:48:13 +0800

    Remove open source content

--- lib/utils/ap_localizations.dart
+++ lib/utils/ap_localizations.dart
@@ -108,9 +108,6 @@ class ApLocalizations {
 
   String get aboutOpenSourceTitle => _vocabularies['about_open_source_title'];
 
-  String get aboutOpenSourceContent =>
-      _vocabularies['about_open_source_content'];
-
   String get back => _vocabularies['back'];
 
   String get people => _vocabularies['people'];
@@ -871,8 +868,6 @@ class ApLocalizations {
       'about_itc_title': 'NKUST IT Club',
       'about_contact_us': 'Contact Us',
       'about_open_source_title': 'Open Source License',
-      'about_open_source_content':
-          'https://github.com/NKUST-ITC/NKUST-AP-Flutter\n\nThis project is licensed under the terms of the MIT license:\nThe MIT License (MIT)\n\nCopyright &#169; 2018 Rainvisitor\n\nThis project is Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
       'open_drawer': 'Open Menu',
       'close_drawer': 'Close Menu',
       'news': 'News',
@@ -1224,8 +1219,6 @@ class ApLocalizations {
       'about_itc_title': '高科資研社',
       'about_contact_us': '聯繫我們',
       'about_open_source_title': '開放原始碼授權',
-      'about_open_source_content':
-          'https://github.com/NKUST-ITC/NKUST-AP-Flutter\n\n本專案採MIT 開放原始碼授權：\nThe MIT License (MIT)\n\nCopyright &#169; 2018 Rainvisitor\n\nThis project is Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
       'open_drawer': '打開功能表',
       'close_drawer': '關閉功能表',
       'news': '最新消息',
