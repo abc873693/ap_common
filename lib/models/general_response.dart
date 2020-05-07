6b284443886a7b94c7873195aae78a2d856c66aa
RainVisitor <abc873693@gmail.com>
2020-05-07 14:46:25 +0800

    Add general response  json parser

--- lib/models/general_response.dart
+++ lib/models/general_response.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 
 class GeneralResponse {
@@ -8,9 +10,26 @@ class GeneralResponse {
     @required this.statusCode,
     @required this.message,
   });
+
   static GeneralResponse success() =>
       GeneralResponse(statusCode: 200, message: '');
 
   static GeneralResponse unknownError() =>
       GeneralResponse(statusCode: 1000, message: '');
+
+  factory GeneralResponse.fromRawJson(String str) =>
+      GeneralResponse.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory GeneralResponse.fromJson(Map<String, dynamic> json) =>
+      GeneralResponse(
+        statusCode: json["code"] == null ? null : json["code"],
+        message: json["description"] == null ? null : json["description"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "code": statusCode == null ? null : statusCode,
+        "description": message == null ? null : message,
+      };
 }
