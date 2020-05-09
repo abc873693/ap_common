aadd0d10c9414f4a2bea4a27a00bbe7c7be6c270
RainVisitor <abc873693@gmail.com>
2020-05-09 19:33:31 +0800

    Add force update config

--- lib/models/version_info.dart
+++ lib/models/version_info.dart
@@ -1,6 +1,7 @@
 class VersionInfo {
   int code;
+  bool isForceUpdate;
   String content;
 
-  VersionInfo({this.code, this.content});
+  VersionInfo({this.code, this.isForceUpdate, this.content});
 }
