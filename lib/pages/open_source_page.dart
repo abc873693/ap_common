f719934e603d827f753f57fef984a834c1af98d0
RainVisitor <abc873693@gmail.com>
2020-05-10 02:09:24 +0800

    Update license

--- lib/pages/open_source_page.dart
+++ lib/pages/open_source_page.dart
@@ -361,10 +361,10 @@ class OpenSourcePageState extends State<OpenSourcePage> {
           "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.",
     ),
     _Item(
-      'flutter_native_image',
+      'flutter_image_compress',
       "MIT License\n" +
           "\n" +
-          "Copyright (c) 2018 Ben Konsemüller\n" +
+          "Copyright (c) 2018 caijinglong\n" +
           "\n" +
           "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
           "of this software and associated documentation files (the \"Software\"), to deal\n" +
@@ -383,6 +383,104 @@ class OpenSourcePageState extends State<OpenSourcePage> {
           "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
           "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
           "SOFTWARE.",
+    ),
+    _Item(
+      'flutter_keyboard_visibility',
+      "The MIT License\n" +
+          "\n" +
+          "Copyright (c) 2006-2018\n" +
+          "All rights reserved.\n" +
+          "\n" +
+          "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+          "of this software and associated documentation files (the \"Software\"), to deal\n" +
+          "in the Software without restriction, including without limitation the rights\n" +
+          "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+          "copies of the Software, and to permit persons to whom the Software is\n" +
+          "furnished to do so, subject to the following conditions:\n" +
+          "\n" +
+          "The above copyright notice and this permission notice shall be included in\n" +
+          "all copies or substantial portions of the Software.\n" +
+          "\n" +
+          "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+          "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+          "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+          "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+          "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+          "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" +
+          "THE SOFTWARE.",
+    ),
+    _Item(
+      'material_design_icons_flutter',
+      "MIT License\n" +
+          "\n" +
+          "Copyright (c) 2018 胖叔叔\n" +
+          "\n" +
+          "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+          "of this software and associated documentation files (the \"Software\"), to deal\n" +
+          "in the Software without restriction, including without limitation the rights\n" +
+          "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+          "copies of the Software, and to permit persons to whom the Software is\n" +
+          "furnished to do so, subject to the following conditions:\n" +
+          "\n" +
+          "The above copyright notice and this permission notice shall be included in all\n" +
+          "copies or substantial portions of the Software.\n" +
+          "\n" +
+          "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+          "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+          "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+          "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+          "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+          "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
+          "SOFTWARE.",
+    ),
+    _Item(
+      'native_pdf_view',
+      "MIT License\n" +
+          "\n" +
+          "Copyright (c) 2019 RBC, Serge Shkurko\n" +
+          "\n" +
+          "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+          "of this software and associated documentation files (the \"Software\"), to deal\n" +
+          "in the Software without restriction, including without limitation the rights\n" +
+          "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+          "copies of the Software, and to permit persons to whom the Software is\n" +
+          "furnished to do so, subject to the following conditions:\n" +
+          "\n" +
+          "The above copyright notice and this permission notice shall be included in all\n" +
+          "copies or substantial portions of the Software.\n" +
+          "\n" +
+          "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+          "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+          "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+          "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+          "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+          "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
+          "SOFTWARE.",
+    ),
+    _Item(
+      'barcode_scan',
+      "The MIT License\n" +
+          "\n" +
+          "Copyright (c) 2020 Julian Finkler https://www.mintware.de\n" +
+          "Copyright (c) 2018-2019 AppTree Software http://www.apptreesoftware.com\n" +
+          "\n" +
+          "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+          "of this software and associated documentation files (the \"Software\"), to deal\n" +
+          "in the Software without restriction, including without limitation the rights\n" +
+          "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+          "copies of the Software, and to permit persons to whom the Software is\n" +
+          "furnished to do so, subject to the following conditions:\n" +
+          "\n" +
+          "The above copyright notice and this permission notice shall be included in\n" +
+          "all copies or substantial portions of the Software.\n" +
+          "\n" +
+          "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
+          "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
+          "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
+          "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
+          "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
+          "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n" +
+          "THE SOFTWARE.",
     ),
   ];
 
