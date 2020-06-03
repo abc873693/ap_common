0af193a62007ab997e0d07fc9f29b59fe644b93b
RainVisitor <abc873693@gmail.com>
2020-06-03 20:23:20 +0800

    Add autofillHints for flutter 1.19 api

--- lib/scaffold/login_scaffold.dart
+++ lib/scaffold/login_scaffold.dart
@@ -67,20 +67,22 @@ class LoginScaffoldState extends State<LoginScaffold> {
     return Scaffold(
       backgroundColor: ApTheme.of(context).blue,
       resizeToAvoidBottomPadding: orientation == Orientation.portrait,
-      body: Container(
-        alignment: Alignment(0, 0),
-        padding: EdgeInsets.symmetric(horizontal: 30.0),
-        child: orientation == Orientation.portrait
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                mainAxisSize: MainAxisSize.min,
-                children: _renderContent(orientation),
-              )
-            : Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: _renderContent(orientation),
-              ),
+      body: AutofillGroup(
+        child: Container(
+          alignment: Alignment(0, 0),
+          padding: EdgeInsets.symmetric(horizontal: 30.0),
+          child: orientation == Orientation.portrait
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: _renderContent(orientation),
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _renderContent(orientation),
+                ),
+        ),
       ),
     );
   }
@@ -260,6 +262,7 @@ class ApTextField extends StatelessWidget {
   final bool obscureText;
   final int maxLength;
   final TextInputType keyboardType;
+  final Iterable<String> autofillHints;
 
   const ApTextField({
     Key key,
@@ -273,6 +276,7 @@ class ApTextField extends StatelessWidget {
     this.obscureText = false,
     this.maxLength,
     this.onChanged,
+    this.autofillHints,
   }) : super(key: key);
 
   @override
@@ -301,6 +305,7 @@ class ApTextField extends StatelessWidget {
         fontSize: 18.0,
         decorationColor: Colors.white,
       ),
+      autofillHints: autofillHints,
     );
   }
 }
