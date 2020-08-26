ffa90bafdbc26e61f8c49abb0e8c20471a3b714e
RainVisitor <abc873693@gmail.com>
2020-08-27 02:27:20 +0800

    Add state for pdf scaffold

--- lib/scaffold/pdf_scaffold.dart
+++ lib/scaffold/pdf_scaffold.dart
@@ -1,32 +1,59 @@
 import 'dart:typed_data';
 
+import 'package:ap_common/resources/ap_icon.dart';
+import 'package:ap_common/utils/ap_localizations.dart';
+import 'package:ap_common/widgets/hint_content.dart';
 import 'package:flutter/material.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 
-class PdfScaffold extends StatefulWidget {
-  final Uint8List byteList;
+enum PdfState { loading, finish, error }
 
-  const PdfScaffold({Key key, this.byteList}) : super(key: key);
+class PdfScaffold extends StatefulWidget {
+  final PdfState state;
+  final Uint8List byteList;
+  final Function onRefresh;
+
+  const PdfScaffold({
+    Key key,
+    @required this.state,
+    @required this.byteList,
+    this.onRefresh,
+  }) : super(key: key);
 
   @override
   _PdfScaffoldState createState() => _PdfScaffoldState();
 }
 
 class _PdfScaffoldState extends State<PdfScaffold> {
-  PdfController pdfController;
-
   @override
   void initState() {
-    pdfController = PdfController(
-      document: PdfDocument.openData(widget.byteList),
-    );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return PdfView(
-      controller: pdfController,
-    );
+    switch (widget.state) {
+      case PdfState.loading:
+        return Container(
+          child: CircularProgressIndicator(),
+          alignment: Alignment.center,
+        );
+      case PdfState.error:
+        return FlatButton(
+          onPressed: () {
+            if (widget.onRefresh != null) widget.onRefresh();
+          },
+          child: HintContent(
+            icon: ApIcon.error,
+            content: ApLocalizations.of(context).clickToRetry,
+          ),
+        );
+      default:
+        return PdfView(
+          controller: PdfController(
+            document: PdfDocument.openData(widget.byteList),
+          ),
+        );
+    }
   }
 }
