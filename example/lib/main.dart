885624b222af442d2bdaa67193340e50ea808c9b
RainVisitor <abc873693@gmail.com>
2020-10-12 21:54:11 +0800

    Remove example debug mode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:ap_common/resources/ap_icon.dart';
 import 'package:ap_common/utils/preferences.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -10,8 +11,7 @@ import 'config/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  bool isInDebugMode = Constants.isInDebugMode;
-  HttpClient.enableTimelineLogging = isInDebugMode;
+  HttpClient.enableTimelineLogging = kDebugMode;
   await Preferences.init(key: Constants.key, iv: Constants.iv);
   ApIcon.code =
       Preferences.getString(Constants.PREF_ICON_STYLE_CODE, ApIcon.OUTLINED);
