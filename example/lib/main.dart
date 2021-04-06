125d2869d0d474d5947bed0e28d23fa6e588861d
RainVisitor <abc873693@gmail.com>
2021-04-07 00:47:09 +0800

    Comment no import package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:ap_common/resources/ap_icon.dart';
 import 'package:ap_common/utils/preferences.dart';
-import 'package:google_sign_in_dartio/google_sign_in_dartio.dart';
+// import 'package:google_sign_in_dartio/google_sign_in_dartio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -14,11 +14,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpClient.enableTimelineLogging = !kIsWeb && kDebugMode;
   await Preferences.init(key: Constants.key, iv: Constants.iv);
-  if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
-    GoogleSignInDart.register(
-        clientId:
-            '141403473068-9gii2blqbggijifq0ijoqkqv8oj2i2ff.apps.googleusercontent.com');
-  }
+  // if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
+  //   GoogleSignInDart.register(
+  //       clientId:
+  //           '141403473068-9gii2blqbggijifq0ijoqkqv8oj2i2ff.apps.googleusercontent.com');
+  // }
   ApIcon.code =
       Preferences.getString(Constants.PREF_ICON_STYLE_CODE, ApIcon.OUTLINED);
   runApp(MyApp());
