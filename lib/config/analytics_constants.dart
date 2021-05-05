2e56e2ae69a76c078cdb80e750e0c7c7e7710c81
RainVisitor <abc873693@gmail.com>
2021-05-05 11:13:54 +0800

    Update config naming by lint hint

--- lib/config/analytics_constants.dart
+++ lib/config/analytics_constants.dart
@@ -1,18 +1,18 @@
 class AnalyticsConstants {
   // user property
-  static const STUDENT_ID = 'student_id';
-  static const DEPARTMENT = 'department';
-  static const CLASS_NAME = 'class_name';
-  static const THEME = 'theme';
-  static const ICON_STYLE = 'icon_style';
-  static const LANGUAGE = 'language';
-  static const HAS_ENABLE_NOTIFICATION = 'has_enable_notification';
+  static const studentId = 'student_id';
+  static const department = 'department';
+  static const className = 'class_name';
+  static const theme = 'theme';
+  static const iconStyle = 'icon_style';
+  static const language = 'language';
+  static const hasEnableNotification = 'has_enable_notification';
 
-  static const YES = 'yes';
-  static const NO = 'no';
+  static const yes = 'yes';
+  static const no = 'no';
 
   //analytics
-  static const String COURSE_HTML_PARSER = 'course_html_parser';
-  static const String SCORE_HTML_PARSER = 'score_html_parser';
-  static const String REFRESH = 'refresh';
+  static const String courseHtmlParser = 'course_html_parser';
+  static const String scoreHtmlParser = 'score_html_parser';
+  static const String refresh = 'refresh';
 }
