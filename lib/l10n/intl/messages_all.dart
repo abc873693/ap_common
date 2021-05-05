60df6d7be07348a774377b8ba58b8121b9ac2348
RainVisitor <abc873693@gmail.com>
2021-05-05 11:20:33 +0800

    Update i10n by lint hint

--- lib/l10n/intl/messages_all.dart
+++ lib/l10n/intl/messages_all.dart
@@ -37,14 +37,13 @@ MessageLookupByLibrary? _findExact(String localeName) {
 
 /// User programs should call this before using [localeName] for messages.
 Future<bool> initializeMessages(String localeName) async {
-  var availableLocale = Intl.verifiedLocale(
-    localeName,
-    (locale) => _deferredLibraries[locale] != null,
-    onFailure: (_) => null);
+  final availableLocale = Intl.verifiedLocale(
+      localeName, (locale) => _deferredLibraries[locale] != null,
+      onFailure: (_) => null);
   if (availableLocale == null) {
     return new Future.value(false);
   }
-  var lib = _deferredLibraries[availableLocale];
+  final lib = _deferredLibraries[availableLocale];
   await (lib == null ? new Future.value(false) : lib());
   initializeInternalMessageLookup(() => new CompositeMessageLookup());
   messageLookup.addLocale(availableLocale, _findGeneratedMessagesFor);
@@ -60,8 +59,8 @@ bool _messagesExistFor(String locale) {
 }
 
 MessageLookupByLibrary? _findGeneratedMessagesFor(String locale) {
-  var actualLocale = Intl.verifiedLocale(locale, _messagesExistFor,
-      onFailure: (_) => null);
+  final actualLocale =
+      Intl.verifiedLocale(locale, _messagesExistFor, onFailure: (_) => null);
   if (actualLocale == null) return null;
   return _findExact(actualLocale);
 }
--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -10,7 +10,7 @@ import 'intl/messages_all.dart';
 
 // ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
 // ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
-// ignore_for_file: avoid_redundant_argument_values
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
 
 class ApLocalizations {
   ApLocalizations();
@@ -18,28 +18,31 @@ class ApLocalizations {
   static ApLocalizations? _current;
 
   static ApLocalizations get current {
-    assert(_current != null, 'No instance of ApLocalizations was loaded. Try to initialize the ApLocalizations delegate before accessing ApLocalizations.current.');
+    assert(_current != null,
+        'No instance of ApLocalizations was loaded. Try to initialize the ApLocalizations delegate before accessing ApLocalizations.current.');
     return _current!;
   }
 
-  static const AppLocalizationDelegate delegate =
-    AppLocalizationDelegate();
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
 
   static Future<ApLocalizations> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
-    final localeName = Intl.canonicalizedLocale(name); 
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
     return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
       final instance = ApLocalizations();
       ApLocalizations._current = instance;
- 
+
       return instance;
     });
-  } 
+  }
 
   static ApLocalizations of(BuildContext context) {
     final instance = ApLocalizations.maybeOf(context);
-    assert(instance != null, 'No instance of ApLocalizations present in the widget tree. Did you add ApLocalizations.delegate in localizationsDelegates?');
+    assert(instance != null,
+        'No instance of ApLocalizations present in the widget tree. Did you add ApLocalizations.delegate in localizationsDelegates?');
     return instance!;
   }
 
