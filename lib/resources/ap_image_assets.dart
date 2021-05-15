2c1a8a73449d9e020e0924d3c17c90bd97852756
RainVisitor <abc873693@gmail.com>
2021-05-16 01:38:03 +0800

    Fix assets path error

--- lib/resources/ap_image_assets.dart
+++ lib/resources/ap_image_assets.dart
@@ -3,14 +3,14 @@ part of 'resources.dart';
 class ApImageAssets {
   ApImageAssets._();
 
-  static const String email = 'assets/images/email.webp';
-  static const String github = 'assets/images/github.webp';
+  static const String email = 'packages/ap_common/assets/images/email.webp';
+  static const String github = 'packages/ap_common/assets/images/github.webp';
   static const String drawerBackgroundLight =
-      'assets/images/drawer_background_light.webp';
+      'packages/ap_common/assets/images/drawer_background_light.webp';
   static const String drawerBackgroundDark =
-      'assets/images/drawer_background_dark.webp';
-  static const String nkutstItc = 'assets/images/nkutst_itc.webp';
-  static const String fb = 'assets/images/fb.webp';
-  static const String dashLineDark = 'assets/images/dash_line_dark.webp';
-  static const String dashLineLight = 'assets/images/dash_line_light.webp';
+      'packages/ap_common/assets/images/drawer_background_dark.webp';
+  static const String nkutstItc = 'packages/ap_common/assets/images/nkutst_itc.webp';
+  static const String fb = 'packages/ap_common/assets/images/fb.webp';
+  static const String dashLineDark = 'packages/ap_common/assets/images/dash_line_dark.webp';
+  static const String dashLineLight = 'packages/ap_common/assets/images/dash_line_light.webp';
 }
