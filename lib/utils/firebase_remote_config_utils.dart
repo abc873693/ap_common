3931ee376b29c9750ce5e85f13db4aee3b5f3008
RainVisitor <abc873693@gmail.com>
2021-05-20 13:30:16 +0800

    Remove deprecated api

--- lib/utils/firebase_remote_config_utils.dart
+++ lib/utils/firebase_remote_config_utils.dart
@@ -5,24 +5,6 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 export 'package:ap_common/models/version_info.dart';
 export 'package:firebase_remote_config/firebase_remote_config.dart';
 
-class FirebaseRemoteConfigUtils {
-  @deprecated
-  static Future<VersionInfo?> getVersionInfo() async {
-    RemoteConfig remoteConfig;
-    try {
-      remoteConfig = RemoteConfig.instance;
-      await remoteConfig.fetch();
-      await remoteConfig.activate();
-      return VersionInfo(
-        code: remoteConfig.getInt(ApConstants.appVersion),
-        isForceUpdate: remoteConfig.getBool(ApConstants.isForceUpdate),
-        content: remoteConfig.getString(ApConstants.newVersionContent),
-      );
-    } catch (exception) {} finally {}
-    return null;
-  }
-}
-
 extension RemoteConfigExtension on RemoteConfig {
   VersionInfo get versionInfo => VersionInfo(
         code: getInt(ApConstants.appVersion),
