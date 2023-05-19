2e03798624e94181664a93aab3b096ac841fc78d
Rainvisitor <abc873693@gmail.com>
2023-05-19 17:56:32 +0800

    fix: child mutable error

--- lib/scaffold/course_scaffold.dart
+++ lib/scaffold/course_scaffold.dart
@@ -474,7 +474,14 @@ class CourseScaffoldState extends State<CourseScaffold> {
     final int maxTimeCode = widget.courseData.maxTimeCodeIndex;
     final int minTimeCode = widget.courseData.minTimeCodeIndex;
     final bool hasHoliday = widget.courseData.hasHoliday;
-    final List<Column> columns = <Column>[const Column()];
+    final List<Column> columns = <Column>[
+      //TODO 可讀性改善
+      //ignore: prefer_const_constructors
+      Column(
+        //ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[],
+      )
+    ];
     columns[0].children.add(
           _weekBorder(''),
         );
