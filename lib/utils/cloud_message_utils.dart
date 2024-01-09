eea6d840d2e0533905c27d4b139b5a9d2994a512
Rainvisitor <abc873693@gmail.com>
2024-01-09 17:30:44 +0800

    feat: remove hive storage cloud message implement

--- lib/utils/cloud_message_utils.dart
+++ lib/utils/cloud_message_utils.dart
@@ -1,22 +1,8 @@
-import 'package:ap_common/utils/ap_hive_utils.dart';
-import 'package:hive/hive.dart';
-
-export 'package:ap_common/models/cloud_message.dart';
-export 'package:hive/hive.dart';
-
 class CloudMessageUtils {
-  static const String boxName = 'RemoteMessageList';
-
   static CloudMessageUtils? _instance;
-
-  Box<CloudMessage> get box => Hive.box<CloudMessage>(boxName);
 
   // ignore: prefer_constructors_over_static_methods
   static CloudMessageUtils get instance {
     return _instance ?? CloudMessageUtils();
-  }
-
-  Future<void> initBox() async {
-    await Hive.openBox<CloudMessage>(boxName);
   }
 }
