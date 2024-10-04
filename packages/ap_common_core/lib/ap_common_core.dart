90f16bc9e38eb10f2d8f3b633f4c46a0a9e2ff95
Rainvisitor <abc873693@gmail.com>
2024-10-04 15:53:37 +0800

    feat: migrate `model`  `util` `config` to new `ap_common_core`

---
+++ packages/ap_common_core/lib/ap_common_core.dart
@@ -0,0 +1,23 @@
+library ap_common_core;
+
+export 'src/config/analytics_constants.dart';
+export 'src/config/ap_constants.dart';
+export 'src/models/announcement_data.dart';
+export 'src/models/announcement_login_data.dart';
+export 'src/models/ap_support_language.dart';
+export 'src/models/course_data.dart';
+export 'src/models/course_notify_data.dart';
+export 'src/models/general_response.dart';
+export 'src/models/imgur_upload_response.dart';
+export 'src/models/notification_data.dart';
+export 'src/models/phone_model.dart';
+export 'src/models/private_cookies_manager.dart';
+export 'src/models/score_data.dart';
+export 'src/models/semester_data.dart';
+export 'src/models/time_code.dart';
+export 'src/models/user_info.dart';
+export 'src/models/version_info.dart';
+export 'src/utilities/analytics_utils.dart';
+export 'src/utilities/crashlytics_utils.dart';
+export 'src/utilities/notification_util.dart';
+export 'src/utilities/preference_util.dart';
