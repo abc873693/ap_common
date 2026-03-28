b14798ebd0b0a2b937b0c15ebf1c5762fc7d849c
Rainvisitor <abc873693@gmail.com>
2026-03-29 01:28:54 +0800

    chore: update contribution

--- packages/ap_common_flutter_core/lib/src/l10n/strings.g.dart
+++ packages/ap_common_flutter_core/lib/src/l10n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 780 (390 per locale)
 ///
-/// Built on 2026-03-28 at 09:44 UTC
+/// Built on 2026-03-28 at 17:24 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
