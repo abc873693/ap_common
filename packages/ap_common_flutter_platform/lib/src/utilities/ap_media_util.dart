ecbf7072188ab6dcc6ce9ba60302e4fcff8f5534
Rainvisitor(Rainer) <abc873693@gmail.com>
2026-04-11 20:10:01 +0800

    refactor(platform): 以 gal 取代 photo_manager 進行圖片儲存 (#166)
    * refactor(platform): replace photo_manager with gal for image saving
    
    Closes #165
    
    * fix(platform): add permission handling and fix GalException error message

--- packages/ap_common_flutter_platform/lib/src/utilities/ap_media_util.dart
+++ packages/ap_common_flutter_platform/lib/src/utilities/ap_media_util.dart
@@ -1,13 +1,12 @@
-import 'dart:developer';
 import 'dart:io';
 
 import 'package:ap_common_flutter_core/ap_common_flutter_core.dart';
 import 'package:file_saver/file_saver.dart';
 import 'package:flutter/foundation.dart';
+import 'package:gal/gal.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
-import 'package:photo_manager/photo_manager.dart';
 
 class ApMediaUtil extends MediaUtil {
   @override
@@ -24,39 +23,43 @@ class ApMediaUtil extends MediaUtil {
     required String fileName,
   }) async {
     try {
-      PermissionState hasGrantPermission = PermissionState.notDetermined;
-      if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
-        hasGrantPermission = await PhotoManager.requestPermissionExtend();
-      } else {
-        hasGrantPermission = PermissionState.authorized;
-      }
-      if (hasGrantPermission == PermissionState.authorized ||
-          hasGrantPermission == PermissionState.limited) {
-        final Uint8List pngBytes = byteData.buffer.asUint8List();
-        String filePath = '$fileName.png';
-        if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
-          final String tempPath = path.join(
-            (await getApplicationDocumentsDirectory()).path,
-            filePath,
-          );
-          final File file = await File(tempPath).writeAsBytes(pngBytes);
-          final AssetEntity imageEntity =
-              await PhotoManager.editor.saveImageWithPath(
-            file.path,
-            title: fileName,
-          );
-          file.delete();
-          if (kDebugMode) log(imageEntity.title ?? '');
-        } else {
-          filePath = await FileSaver.instance.saveFile(
-            name: '$fileName.png',
-            bytes: pngBytes,
-          );
+      final Uint8List pngBytes = byteData.buffer.asUint8List();
+      final String filePath = '$fileName.png';
+      if (!kIsWeb &&
+          (Platform.isAndroid ||
+              Platform.isIOS ||
+              Platform.isMacOS ||
+              Platform.isWindows)) {
+        if (!await Gal.hasAccess(toAlbum: true)) {
+          if (!await Gal.requestAccess(toAlbum: true)) {
+            return const SaveImageError(
+              'permission_denied',
+            );
+          }
+        }
+        final String tempPath = path.join(
+          (await getTemporaryDirectory()).path,
+          filePath,
+        );
+        final File file =
+            await File(tempPath).writeAsBytes(pngBytes);
+        try {
+          await Gal.putImage(file.path, album: 'AP');
+        } finally {
+          await file.delete();
         }
         return SaveImageSuccess(filePath);
       } else {
-        return const SaveImageError('permission_denied');
+        final String savedPath =
+            await FileSaver.instance.saveFile(
+          name: '$fileName.png',
+          bytes: pngBytes,
+        );
+        return SaveImageSuccess(savedPath);
       }
+    } on GalException catch (e, s) {
+      CrashlyticsUtil.instance.recordError(e, s);
+      return SaveImageError(e.type.name);
     } catch (e, s) {
       CrashlyticsUtil.instance.recordError(e, s);
       return SaveImageError(e.toString());
