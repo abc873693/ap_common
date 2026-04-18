b0599b9787737bf5f913e2ae04ca32199227b536
Rainvisitor <abc873693@gmail.com>
2026-04-18 14:39:33 +0800

    chore(example): regenerate plugin registrants and refresh pubspec.lock

--- apps/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,10 +8,10 @@ import Foundation
 import file_saver
 import file_selector_macos
 import flutter_local_notifications
+import gal
 import google_sign_in_ios
 import in_app_review
 import package_info_plus
-import photo_manager
 import printing
 import share_plus
 import shared_preferences_foundation
@@ -23,10 +23,10 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- apps/example/windows/flutter/generated_plugins.cmake
+++ apps/example/windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   file_saver
   file_selector_windows
+  gal
   printing
   share_plus
   url_launcher_windows
